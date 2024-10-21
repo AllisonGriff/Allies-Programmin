23e478ea6ab02c832235ee78bd823b2511744d44
AllisonGriff <gallie8699@gmail.com>
2024-10-21 08:05:09 -0400

    updating with classrom work

---
+++ Indiana University Code/I101/i101/py2.py
@@ -0,0 +1,72 @@
+# # # # # # # # len("Chase")
+# # # # # # # # print(len("Chase"))
+
+# # # # # # # day = "Thursday"
+# # # # # # # print(len("katie"))
+# # # # # # # print(len("katie, chase, logan"))
+# # # # # # # print(len(day))
+
+# # # # # # print("Informatics!"[0])
+# # # # # # print("Informatics!"[4])
+# # # # # # print("Informatics!"[11])
+
+# # # # # # course = "Info I 101"
+# # # # # # print(course[0:4:1])
+
+# # # # # names = ["Logan", "Chase","Katie"]
+# # # # # print(names[0])
+# # # # # print(names[1])
+# # # # # print(names[2])
+
+# # # # names = ["Logan","Chase","Katie"]
+# # # # print.append("Ahlee")
+# # # # print(names)
+# # # # names.remove("Logan")
+# # # # print(names)
+
+# # # # if 5 == 5:
+# # # #     print("yes")
+
+# # # # names = ["Logan","Chase","Katie"]
+# # # # print(names[0]=="Chase")
+
+# # # # if 4 > 5:
+# # # #     print("this text will never show")
+# # # # if "i101" != "i210":
+# # # #     print("does not equal")
+
+# # # #     x=5
+# # # #     y=10
+# # # #     if x < y:
+# # # #         print("x is less than y")
+
+# # # x=50 
+# # # y=150
+# # # z=200
+# # # if x<y:
+# # #     print("x is greater than y")
+# # # if y>x:
+# # #     print(z)
+# # # if z==(x+y):
+# # #     print("z is the sum of x+y")
+# # #     print(str(z)+"is the sum of" +str(x+y))
+
+# # x=5
+# # y=10
+# # if x<10 and y<10:
+# #     print("both nums are less than 10")
+# # if x<10 or y<10:
+# #     print("one or both numbers are less than 10")
+
+# charCount = len("I learned a lot of Python")
+# if charCount > 10:
+#     print("i learned a lot of python")
+# else:
+#     print("there are less than 10 char")
+charCount = len("Python")
+if charCount > 15:
+    print("i learned a lot of python")
+elif charCount>=10 and charCount <=15:
+    print("there a between 10 and 15 char")
+else:
+    print("there are less than 10 char")
